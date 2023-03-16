7f8a8c74e6b29219bc6d743bf4a689ff67d06fe9
mahmoudadelaziz <mahmoudaziz501@gmail.com>
2023-03-16 17:40:08 +0200

    Removed some unnecessary ids from the HTML and CSS.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // grab the calculator screen
 const myDisplay = document.getElementById("display")
 
+// Define a function to clear the calculator screen
+function cleardisplay() {myDisplay.value = ""}
+
 // Make an array of Number buttons
 let NumButtons = Array.from(document.querySelectorAll(".Nums"))
 
@@ -13,8 +16,20 @@ for(let i = 0; i < NumButtons.length; i++)
     NumButtons[i].addEventListener("click", (event)=>{myDisplay.value += event.target.innerText})
 }
 
+// Make an array of Operation buttons
+let OpButtons = Array.from(document.querySelectorAll(".Ops"))
+
+// Give special event listeners to Number buttons
+for(let i = 0; i < OpButtons.length; i++)
+{
+    // Log the number clicked (for debugging)
+    OpButtons[i].addEventListener("click", (event)=>{console.log(event.target.innerText)})
+    // Add the required behavior (1. clear screen, 2. expect new input, 3. add operand and operator to array)
+    OpButtons[i].addEventListener("click", cleardisplay)
+
+}
+
 // Enable the functionality of the C "Clear" Button
-function cleardisplay() {myDisplay.value = ""}
 document.getElementById("C").addEventListener("click", cleardisplay)
 
 // Backspace functionality
