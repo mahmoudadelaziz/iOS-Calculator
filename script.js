394bea9377014b283574ee2c2f160f20d51c9e97
mahmoudadelaziz <mahmoudaziz501@gmail.com>
2023-03-16 16:33:51 +0200

    Refined the functionality of the Number buttons, and implemented a Clear button (C).

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
+// grab the calculator screen
 const myDisplay = document.getElementById("display")
-
-// initial message
-// myDisplay.innerText = 0
 
 // Make an array of Number buttons
 let NumButtons = Array.from(document.querySelectorAll(".Nums"))
 
+// Give special event listeners to Number buttons
 for(let i = 0; i < NumButtons.length; i++)
 {
+    // Log the number clicked (for debugging)
     NumButtons[i].addEventListener("click", (event)=>{console.log(event.target.innerText)})
-    NumButtons[i].addEventListener("click", (event)=>{myDisplay.value = event.target.innerText})
+    // Show the number clicked on the screen
+    NumButtons[i].addEventListener("click", (event)=>{myDisplay.value += event.target.innerText})
 }
 
-// document.querySelector(".Nums").addEventListener("click", console.log("A Number was Clicked!"))
+// Enable the functionality of the C "Clear" Button
+function cleardisplay() {myDisplay.value = ""}
+document.getElementById("C").addEventListener("click", cleardisplay)
