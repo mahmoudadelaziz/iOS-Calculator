e3de11684027a8205daf21959d4d9f1dc55e1a9c
mahmoudadelaziz <mahmoudaziz501@gmail.com>
2023-03-16 15:35:40 +0200

    Added a special event listener to all Number Buttons.

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 const myDisplay = document.getElementById("display")
-myDisplay.innerText = "Hello!"
+
+// initial message
+// myDisplay.innerText = 0
+
+// Make an array of Number buttons
+let NumButtons = Array.from(document.querySelectorAll(".Nums"))
+
+for(let i = 0; i < NumButtons.length; i++)
+{
+    NumButtons[i].addEventListener("click", (event)=>{console.log(event.target.innerText)})
+    NumButtons[i].addEventListener("click", (event)=>{myDisplay.value = event.target.innerText})
+}
+
+// document.querySelector(".Nums").addEventListener("click", console.log("A Number was Clicked!"))
